2fcee7791514f06e6faf19481f3cfc56a28c2b40
Hridayansh018 <hridayansh018@gmail.com>
2024-09-04 17:49:31 +0530

    update

--- frontend/src/components/SearchBar.jsx
+++ frontend/src/components/SearchBar.jsx
@@ -1,15 +1,23 @@
-import React, { useContext } from 'react'
-import { ShopContext } from '../context/ShopContext'
+import { useContext } from 'react';
+import { ShopContext } from '../context/ShopContext';
 
 const SearchBar = () => {
-
-    const {search, setSearch, showSearch, setshowSearch} = useContext(ShopContext)
+  const { search, setSearch, showSearch, setshowSearch } = useContext(ShopContext);
 
   return showSearch ? (
-    <div className=''>
-      
+    <div className={`border-t border-b bg-gray-50 text-center mt-12 ${showSearch ? 'block' : 'hidden'}`}>
+      <div className="inline-flex items-center border border-gray-300 px-5 py-2 my-5 mx-3 rounded-full w-3/4 sm:1/2">
+        {/* <input 
+          type="text" 
+          placeholder='Search' 
+          className='outline-none bg-transparent w-full' 
+          value={search} 
+          onChange={(e) => setSearch(e.target.value)}
+          onBlur={() => setshowSearch(false)}
+        /> */}
+      </div>
     </div>
-  ) : null
+  ) : null;
 }
 
-export default SearchBar
+export default SearchBar;
